150cc478e96e0409a8f0dbaca69fe7f09c1d6891
ahmedelsewailky <ahmede.lsewailky@gmail.com>
2024-01-10 11:37:19 +0200

    Register dashboard routes file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -20,6 +20,13 @@ class RouteServiceProvider extends ServiceProvider
     public const HOME = '/home';
 
     /**
+     * The controller namespace for the application.
+     *
+     * @var string|null
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
     public function boot(): void
@@ -34,7 +41,13 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->prefix('panel')
+                ->namespace($this->namespace . '\Dashboard')
+                ->group(base_path('routes/dashboard.php'));
         });
     }
 }
