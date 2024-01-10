1b537c37d5a79d731c18b09850db2cb1cca8b554
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2024-01-10 19:48:16 +0200

    Category complete controller, request, model, seeder and migration

---
+++ app/Models/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'name', 'slug', 'parent_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<string>
+     */
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    /**
+     * Bootstrap the model and its traits.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function($category) {
+            return $category->slug = str()->slug($category->name);
+        });
+    }
+}
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $mainCategories = [
+            'افلام',
+            'برامج',
+            'مسلسلات',
+            'بودكاست',
+        ];
+
+        foreach ($mainCategories as $category) {
+            \App\Models\Category::create([
+                'name' => $category,
+                'slug' => str()->slug($category)
+            ]);
+        }
+    }
+}
