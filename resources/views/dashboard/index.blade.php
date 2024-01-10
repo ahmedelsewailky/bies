7cc0bf30484dfccbc88e42ca981aace6cebbce00
ahmedelsewailky <ahmede.lsewailky@gmail.com>
2024-01-10 11:38:05 +0200

    Init dashboard index file created

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+{{-- Extend master layout --}}
+@extends('layouts.app')
+
+{{-- Define page title --}}
+@section('title', __('Dashboard'))
+
+{{-- Page content --}}
+@section('content')
+    <h2>Dashboard Welcome!</h2>
+@endsection
