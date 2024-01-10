5ec22736855e4865644ac766f0b972f3573a1e5f
ahmedelsewailky <ahmede.lsewailky@gmail.com>
2024-01-10 10:10:41 +0200

    Create seeder for master admin

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'name' => 'Ahmed Elsewailky',
+            'username' => 'admin',
+            'email' => 'admin@yahoo.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin'),
+            'remember_token' => str()->random(10),
+        ]);
+    }
+}
